6c484682c5de88b8c5daaa1dbcd3d842f6dbbdf7
Jordan Giacone <jmgiacone@comcast.net>
2013-01-26 03:23:21 -0600

    Initial commit of Plugin Skeleton

---
+++ src/com/github/jdog653/publicexecution/PublicExecution.java
@@ -0,0 +1,29 @@
+package com.github.jdog653.publicexecution;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * @author Jdog653
+ */
+public class PublicExecution extends JavaPlugin
+{
+    @Override
+    public void onEnable()
+    {
+
+    }
+
+    @Override
+    public void onDisable()
+    {
+
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args)
+    {
+        return true;
+    }
+}
